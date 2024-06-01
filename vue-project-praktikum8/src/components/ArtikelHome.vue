f17289a51a0493ab819e21e615d0c80cac215661
VinDicky <kevindicky11@students.amikom.ac.id>
2024-06-01 19:10:24 +0700

    praktikum 8

---
+++ vue-project-praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1> Ini adalah komponen </h1>
+        <p> Ini adalah komponen </p>
+        <p> Ini adalah komponen </p>
+        <p> Ini adalah komponen </p>
+    </div>
+</template>
---
+++ vue-project-praktikum8/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import ArtikelHome from '../components/ArtikelHome.vue'
+</script>
+
+<template>
+  <main>
+    <ArtikelHome/>
+  </main>
+</template>
